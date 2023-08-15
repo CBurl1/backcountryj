5bf8dd31e11e7838758a5a3a9bc71c00092ccc6b
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-08-15 00:00:27 -0400

    restructure

--- backcountryj/src/Forest.js
+++ backcountryj/src/Forest.js
@@ -1,43 +1,55 @@
-import { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
-const Forest = ({ width = 800, height = 600 }) => {
+const SnowForestBackground = ({ width, height }) => {
   const canvasRef = useRef(null);
   const [forestPattern, setForestPattern] = useState([]);
 
-  const drawTree = (context, x, y, treeWidth, treeHeight) => {
+  const drawSnowBackground = (context) => {
+    const gradient = context.createLinearGradient(0, 0, width, height);
+    gradient.addColorStop(0, '#f0f0f0');
+    gradient.addColorStop(0.5, '#e6e6e6');
+    gradient.addColorStop(1, '#f0f0f0');
+
+    context.fillStyle = gradient;
+    context.fillRect(0, 0, width, height);
+  };
+
+  const drawTree = (context, x, y, width, height) => {
     const levels = 3;
     for (let i = 0; i < levels; i++) {
-      context.fillStyle = '#228B22'; // Green color for the leaves
+      context.fillStyle = '#228B22';
       context.beginPath();
-      context.moveTo(x + treeWidth / 2, y + (i * treeHeight) / (levels + 1));
-      context.lineTo(x, y + ((i + 1) * treeHeight) / (levels + 1));
-      context.lineTo(x + treeWidth, y + ((i + 1) * treeHeight) / (levels + 1));
+      context.moveTo(x + width / 2, y + (i * height) / (levels + 1));
+      context.lineTo(x, y + ((i + 1) * height) / (levels + 1));
+      context.lineTo(x + width, y + ((i + 1) * height) / (levels + 1));
       context.closePath();
       context.fill();
     }
     context.fillStyle = '#8B4513'; // Brown color for the trunk
-    context.fillRect(x + treeWidth / 3, y + (3 * treeHeight) / 4, treeWidth / 3, treeHeight / 4);
+    context.fillRect(x + width / 3, y + (3 * height) / 4, width / 3, height / 4);
+    context.strokeStyle = '#5A2D0C'; // Darker brown for the trunk lines
+    context.beginPath();
+    context.moveTo(x + width / 3, y + (3 * height) / 4);
+    context.lineTo(x + width / 3, y + height);
+    context.moveTo(x + 2 * width / 3, y + (3 * height) / 4);
+    context.lineTo(x + 2 * width / 3, y + height);
+    context.stroke();
   };
 
   const drawForest = (context) => {
     const treeWidth = 40;
     const treeHeight = 40;
 
-    forestPattern.forEach((variation, index) => {
-      const y = index * treeHeight;
-      for (let x = 0; x < variation; x += treeWidth) {
-        drawTree(context, x, y, treeWidth, treeHeight);
-      }
-      for (let x = width - variation; x < width; x += treeWidth) {
-        drawTree(context, x, y, treeWidth, treeHeight);
-      }
-    });
+
+  const draw = (context) => {
+    drawSnowBackground(context);
+    drawForest(context);
   };
 
   const generateForestPattern = () => {
     const pattern = [];
     for (let i = 0; i < 30; i++) {
-      pattern.push(Math.random() * 80 + 40); // Increased width variation
+      pattern.push(Math.random() * 200 + 40);
     }
     setForestPattern(pattern);
   };
@@ -46,10 +58,10 @@ const Forest = ({ width = 800, height = 600 }) => {
     generateForestPattern();
     const forestInterval = setInterval(() => {
       setForestPattern((prevPattern) => {
-        const newPattern = [...prevPattern.slice(1), Math.random() * 80 + 40];
+        const newPattern = [...prevPattern.slice(1), Math.random() * 200 + 40];
         return newPattern;
       });
-    }, 2000);
+    }, 300);
 
     return () => clearInterval(forestInterval);
   }, []);
@@ -57,12 +69,9 @@ const Forest = ({ width = 800, height = 600 }) => {
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
-    const draw = () => {
-      drawForest(context);
-    };
 
     const interval = setInterval(() => {
-      draw();
+      draw(context);
     }, 100);
 
     return () => clearInterval(interval);
@@ -71,4 +80,4 @@ const Forest = ({ width = 800, height = 600 }) => {
   return <canvas ref={canvasRef} width={width} height={height} />;
 };
 
-export default Forest;
+export default SnowForestBackground;
--- backcountryj/src/GameView.js
+++ backcountryj/src/GameView.js
@@ -2,22 +2,17 @@ import React from 'react';
 import Forest from './Forest.js';
 import SnowBackground from './SnowBackground.js';
 
-const MainGame = () => {
-  const width = 800;
-  const height = 600;
-
+const GameView = () => {
   return (
-    <div style={{ position: 'relative', width: `${width}px`, height: `${height}px` }}>
+    <div>
       <h1>Welcome to Ski Adventure</h1>
-      <SnowBackground width={width} height={height} />
-      <Forest width={width} height={height} />
+      <SnowBackground width={800} height={600} />
+      <Forest />
     </div>
   );
 };
 
-export default MainGame;
-
-
+export default GameView;
 
 
 
